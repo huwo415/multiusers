49c517561f62e1635a9ab65f2df9333e20891a10
huwo415 <huw04153766@gmail.com>
2026-07-23 21:09:14 +0800

    update classA

--- ClassA.java
+++ ClassA.java
@@ -2,6 +2,10 @@ public class ClassA {
     private String name;
     private int version;
 
+    public ClassA(){
+        
+    }
+
     public ClassA(String name, int version) {
         this.name = name;
         this.version = version;
